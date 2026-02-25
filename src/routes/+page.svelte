4fbc8bfb88dfb22db482ec914d62edd01a125fc9
Anni <annemarie.wisbar@haw-hamburg.de>
2026-02-25 14:43:28 +0100

    Install d3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import IconGrid from '$lib/IconGrid.svelte';
+	import D3Chart from '$lib/D3Chart.svelte';
 </script>
 
 <h1>Icon Dashboard</h1>
 <p>Select how many icons to display and view them below.</p>
 
 <IconGrid />
+
+<D3Chart count={50} />
